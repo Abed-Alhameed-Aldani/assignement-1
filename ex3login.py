47f31ba423b789e688b96f31f4c85fee7af7423b
Abed-Aldani <abedaldani2@gmail.com>
2025-02-14 00:26:37 +0200

    printing if access granted/denied by comparing name and password

---
+++ ex3login.py
@@ -0,0 +1,7 @@
+name = input("Please enter ur name: ")
+password = input("Please enter ur password: ")
+
+if name == "admin" and password == "1234" : #tried to use + but it didn't work , searched for the reason and it had to be and
+    print("Access Granted")
+else : 
+    print("access denied")
