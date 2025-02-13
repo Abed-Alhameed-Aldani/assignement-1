dcc34fd397c9c879e87e4b1d8f99c8b1a3d37b55
Abed-Aldani <abedaldani2@gmail.com>
2025-02-13 23:36:06 +0200

    done doing the if statements with the money

--- begin.py
+++ begin.py
@@ -1,5 +1,13 @@
 age = float(input("please enter your age "))
 
+if age >= 10:
+    print ("the ticket price is 10$")
+elif age >= 8:
+   print ("the ticket price is 7$")
+elif age >= 5:
+    print ("the ticket price is 5$")
+elif age <5 :
+ print("You can join for free")
 
 
 
